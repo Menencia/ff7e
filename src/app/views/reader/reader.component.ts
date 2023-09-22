f39363076b165f9e9365413810b77e459c4ffb46
Menencia <menencia@gmail.com>
2023-09-22 20:53:59 +0200

    wip

--- src/app/views/reader/reader.component.ts
+++ src/app/views/reader/reader.component.ts
@@ -4,7 +4,8 @@ import { StoreService } from 'src/app/shared/services/store';
 
 @Component({
   selector: 'app-reader',
-  templateUrl: './reader.component.html'
+  templateUrl: './reader.component.html',
+  styleUrls: ['./reader.component.scss']
 })
 export class ReaderComponent implements OnInit {
 
@@ -18,7 +19,7 @@ export class ReaderComponent implements OnInit {
     this.store.getChapter(0).subscribe(chapter => {
       if (!!chapter) {
         console.log('get')
-        this.content = chapter.content;
+        this.content = this.formatContent(chapter.content);
       } else {
         console.log('load')
         this.loadChapter(0);
@@ -30,17 +31,28 @@ export class ReaderComponent implements OnInit {
     const key = 'AIzaSyA5nfdCODe1LJX5fbwOmwpbBwN-SEzul9I';
     const chapterId = '14l3Qc4Te3cMgPWfZM2qFOd7mjx_fS8yCYHqnYMNgFcA';
     const url = `https://content.googleapis.com/drive/v3/files/${chapterId}/export`;
-    this.http.get(url, {params: {
-      mimeType: 'text/plain',
-      key
-    }, responseType: 'text'}).subscribe((data: string) => {
-      this.store.setChapter(0, data);
-      const lines = data.split('\r\n');
-      lines.shift(); // remove chapter
-      lines.shift(); // remove title
-      const final = '<p>' + lines.join('</p><p>') + '</p>';
-      this.content = final;
-    });
+    this.http.get(url, { params: { mimeType: 'text/plain', key }, responseType: 'text' })
+      .subscribe((data: string) => {
+        this.store.setChapter(0, data);
+        this.content = this.formatContent(data);
+      });
   }
 
+  public formatContent(data: string): string {
+    const lines = data.split('\r\n');
+    lines.shift(); // remove chapter
+    lines.shift(); // remove title
+    const cuts = [6];
+    const parts = [];
+    const part = [];
+    lines.slice(i, i+1)
+    lines.forEach((line, lineNo) => {
+      if (cuts.includes(lineNo)) {
+
+      }
+      part.push(line);
+
+    });
+    return '<p>' + lines.join('</p><p>') + '</p>';
+  }
 }
