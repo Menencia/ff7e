5ac9ea21a534f12a33a7cafa2c8820068407bca7
Menencia <menencia@gmail.com>
2025-09-21 15:54:43 +0200

    feat(reader): add 3 first pages to glossary

--- src/app/views/reader/glossary-word/glossary-word.component.ts
+++ src/app/views/reader/glossary-word/glossary-word.component.ts
@@ -1,8 +1,38 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ReaderLayoutComponent } from 'src/app/shared/ui/reader-layout/reader-layout.component';
+
+interface Word {
+  notes: Note[];
+}
+
+interface Note {
+  condition: string;
+  content: string;
+}
 
 @Component({
   selector: 'app-glossary-word',
+  imports: [ReaderLayoutComponent],
   templateUrl: './glossary-word.component.html',
   styleUrls: ['./glossary-word.component.scss'],
 })
-export class GlossaryWordComponent {}
+export class GlossaryWordComponent implements OnInit {
+  word?: string;
+  notes: Note[] = [];
+
+  constructor(
+    private route: ActivatedRoute,
+    private http: HttpClient,
+  ) {}
+
+  ngOnInit() {
+    this.word = this.route.snapshot.paramMap.get('word') ?? '';
+    this.http
+      .get<Word>(`assets/glossary/${this.word}.json`)
+      .subscribe((data) => {
+        this.notes = data.notes;
+      });
+  }
+}
--- src/app/views/reader/glossary/glossary.component.ts
+++ src/app/views/reader/glossary/glossary.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { ReaderLayoutComponent } from 'src/app/shared/ui/reader-layout/reader-layout.component';
+
+interface Glossary {
+  characters: string[];
+  locations: string[];
+}
 
 @Component({
   selector: 'app-glossary',
-  imports: [ReaderLayoutComponent],
+  imports: [ReaderLayoutComponent, RouterLink],
   templateUrl: './glossary.component.html',
   styleUrls: ['./glossary.component.scss'],
 })
-export class GlossaryComponent {}
+export class GlossaryComponent implements OnInit {
+  characters: string[] = [];
+  locations: string[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get<Glossary>('assets/glossary/_main.json').subscribe((data) => {
+      this.characters = data.characters;
+      this.locations = data.locations;
+    });
+  }
+}
