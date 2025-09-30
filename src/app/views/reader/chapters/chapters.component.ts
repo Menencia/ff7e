39a7adfee7b4ab1f0f0695ff6c904990b11ddc9e
Menencia <menencia@gmail.com>
2025-09-30 22:21:16 +0200

    reader: chapters list indicates curent progress

--- src/app/views/reader/chapters/chapters.component.ts
+++ src/app/views/reader/chapters/chapters.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { Chapter } from 'src/app/shared/models/chapter';
+import { Progress } from 'src/app/shared/models/reader';
 import { ChaptersService } from 'src/app/shared/services/chapters.service';
+import { SaveService } from 'src/app/shared/services/save.service';
 import OptionComponent from 'src/app/shared/ui/option/option.component';
 import { ReaderLayoutComponent } from 'src/app/shared/ui/reader-layout/reader-layout.component';
 
@@ -11,14 +13,21 @@ import { ReaderLayoutComponent } from 'src/app/shared/ui/reader-layout/reader-la
   imports: [ReaderLayoutComponent, OptionComponent, RouterLink],
   templateUrl: './chapters.component.html',
 })
-export class ReaderChaptersComponent {
+export class ReaderChaptersComponent implements OnInit {
   chapters: Chapter[] = [];
+  progress?: Progress;
 
   faBars = faBars;
 
-  constructor(private chaptersService: ChaptersService) {
+  constructor(
+    private chaptersService: ChaptersService,
+    private saveService: SaveService,
+  ) {}
+
+  ngOnInit() {
     this.chaptersService.chapters$.subscribe((chapters) => {
       this.chapters = chapters.filter((e) => e.reader);
     });
+    this.progress = this.saveService.getMaxProgress();
   }
 }
